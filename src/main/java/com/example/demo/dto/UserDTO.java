91eebe54f9a47cabb98af9f80226f01ef6bfa40d
yyujjin <aba1740@naver.com>
2024-08-15 16:38:14 +0900

    :sparkles: UserDTO 생성

---
+++ src/main/java/com/example/demo/dto/UserDTO.java
@@ -0,0 +1,52 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private Long id;
+    private String name;
+    private String eamil;
+    private String picture;
+    private String role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEamil() {
+        return eamil;
+    }
+
+    public void setEamil(String eamil) {
+        this.eamil = eamil;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
